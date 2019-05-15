88a1a9ecf795da5a5e206a56af1f3f07fb77b32f
Julian Vargas <julianvargasalvarez@gmail.com>
2019-05-15 14:43:58 -0500

    adds implementation for statistics in the thermostat model

--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,6 +1,40 @@
 class Thermostat < ApplicationRecord
   include Redis::Objects
+  after_create :initialize_stats
+
   has_many :readings
-  counter :total_measures
-  hash_key :stats
+  counter :total_readings
+
+  value :current_temperature
+  value :min_temperature
+  value :max_temperature
+  value :sum_temperature
+
+  value :current_humidity
+  value :min_humidity
+  value :max_humidity
+  value :sum_humidity
+
+  value :current_battery_charge
+  value :min_battery_charge
+  value :max_battery_charge
+  value :sum_battery_charge
+
+  private
+  def initialize_stats
+    self.current_temperature.value = 0.0
+    self.min_temperature.value = 1000.0
+    self.max_temperature.value =    0.0
+    self.sum_temperature.value =    0.0
+
+    self.current_humidity.value = 0.0
+    self.min_humidity.value    = 1000.0
+    self.max_humidity.value    =    0.0
+    self.sum_humidity.value    =    0.0
+
+    self.current_battery_charge.value = 0.0
+    self.min_battery_charge.value = 1000.0
+    self.max_battery_charge.value =    0.0
+    self.sum_battery_charge.value =    0.0
+  end
 end
